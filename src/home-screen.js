35b72b5e64b9476055ec4263340882bb2c6eabe4
Timofti Gabriel <gabitim470@gmail.com>
2022-06-02 14:34:34 +0300

    Simple backend test + full app deploy

--- src/home-screen.js
+++ src/home-screen.js
@@ -1,13 +1,47 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import {SafeAreaView, StatusBar, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  ActivityIndicator,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  Button,
+} from 'react-native';
+import {API} from 'aws-amplify';
+
+const myAPI = 'api0a629dfb';
+const path = '/translate';
 
 const HomeScreen = (props) => {
+
+  const [loading, setLoading] = useState(false);
+  const [output, setOutput] = useState('');
+
+  const callBackEnd = async () => {
+    setLoading(true);
+    try {
+      const res = await API.post(myAPI, path);
+      setLoading(false);
+      console.log(res);
+      setOutput(res);
+    }
+    catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <Text>Home</Text>
+
+        <Button onPress={() => callBackEnd()} title='Test backend' />
+        <ActivityIndicator animating={loading} size='large' color='#000000' />
+
+        <Text>{output}</Text>
+
       </SafeAreaView>
     </>
   );
